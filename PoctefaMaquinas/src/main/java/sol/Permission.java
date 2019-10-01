ad8f3fd49117a542c29d9ed332c9a19d8e42be81
info1 <info1@info1-HP-EliteBook-840-G2>
2019-10-01 15:06:56 +0200

    <version 3> comienzo tratamiento

---
+++ PoctefaMaquinas/src/main/java/sol/Permission.java
@@ -0,0 +1,37 @@
+package sol;
+import java.util.ArrayList;
+
+public class Permission {
+	//Attributes
+	private ArrayList<String> codes;
+	private boolean coincide;
+
+	//Getters and Setters
+	public boolean isPermission(Student student) {
+		for(int i = codes.size();i>0; --i) {
+			if(codes.get(i).equals(student.code)) {
+				return student.getPermission();
+			}
+			
+		}
+		
+		return false;
+		
+	}
+
+	public void setPermission(Student student) {
+		boolean coincide = false; 
+		
+		for(int i = codes.size(); !coincide && i>0; --i) {
+			if(codes.get(i).equals(student.code)) {
+				coincide = true;
+				student.setPermission(true);
+			}else student.setPermission(false);
+		}
+		
+			
+	}
+	
+	
+
+}
